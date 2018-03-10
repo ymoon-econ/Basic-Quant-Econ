9b58459478a99b3f21452febda047a40fb2f9dc8
hzahidecon <36344515+hzahidecon@users.noreply.github.com>
2018-03-10 11:06:38 -0600

    Create func1.m

---
+++ PS7/Problem1/func1.m
@@ -0,0 +1,22 @@
+function [afinal, a, c] = func1(a1)            
+
+
+global a0 beta sigma r w e T
+
+a = zeros(1, T+2);
+a(1,1) = a0;
+a(1,2) = a1;
+
+c = zeros(1, T);
+
+
+for t=3:T+2
+    
+    a(1,t) = (1+r)*a(1,t-1) + w*e(1,t-1)...
+       - ((beta*(1+r))^(1/sigma))*((1+r)*a(1,t-2) + w*e(1,t-2) - a(1,t-1));
+    
+    c(1,t-2) = (1+r)*a(1,t-2) + w*e(1,t-2) - a(1,t-1);    
+
+end
+
+afinal = a(1,T+2);
